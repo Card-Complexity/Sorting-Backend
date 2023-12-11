762930438be56e6d76a91ad2004223e90e9664de
Ekamjot-Kaire <ekamjot.kaurr@gmail.com>
2023-12-10 16:00:23 -0800

    removing CORS override on controller

--- src/main/java/com/nighthawk/spring_portfolio/mvc/Sorting_APIs/SortsApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/Sorting_APIs/SortsApiController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin()
 @RequestMapping("/api/sorts")
 public class SortsApiController {
     private final SortingService sortingService;
